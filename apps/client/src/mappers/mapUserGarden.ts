5d8113d0bd2531d7b6bbac60b3b4501809352e04
vitalii-zavhorodnii <vince.kkr@gmail.com>
2024-04-26 18:32:40 +0200

    refactored invalid interfaces

--- apps/client/src/mappers/mapUserGarden.ts
+++ apps/client/src/mappers/mapUserGarden.ts
@@ -2,10 +2,10 @@ import { mapFieldRows } from './mapFieldRows';
 
 import { ROW_MAP } from '@constants/rows.constants';
 
-import { IGardenCellSprite } from 'src/interfaces/IPlantData';
 import { IGardenFetched } from '@interfaces/IGardenFetched';
+import { IPlantData } from '@interfaces/IPlantData';
 
-export const userGardenMapper = (field: IGardenFetched): IGardenCellSprite[][] => {
+export const userGardenMapper = (field: IGardenFetched): IPlantData[][] => {
   const data = field.map((item, rowIndex: number) => {
     const concatCoordsFetched = mapFieldRows(ROW_MAP);
 
