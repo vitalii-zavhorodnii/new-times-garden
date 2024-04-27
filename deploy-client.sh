9c2e29176165e0cf20b24c903ebed8807c309c33
vitalii-zavhorodnii <vince.kkr@gmail.com>
2024-04-27 15:00:38 +0200

    add script for deploy

--- deploy-client.sh
+++ deploy-client.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# 1. Отменить все изменения в репозитории с удалением новых файлов, если такие были
 git reset --hard HEAD
 git clean -df
-
-# 2. Стянуть изменения с репозитория
 git pull origin main
 
-# 3. Создать билд командой npm run build:client
 npm run build:client
 
-# 4. Переместить созданный билд клиента в папку apps/api/static
+rm -rf apps/api/static/*
 mv apps/client/dist/* apps/api/static/
 
 echo "All done!"
