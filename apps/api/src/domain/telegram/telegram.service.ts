68c3743a006bb36b80d5a6a3edae2d4bad8c1def
vitalii-zavhorodnii <vince.kkr@gmail.com>
2024-04-27 03:42:55 +0200

    prep for deploy

--- apps/api/src/domain/telegram/telegram.service.ts
+++ apps/api/src/domain/telegram/telegram.service.ts
@@ -1,23 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import * as TelegramBot from 'node-telegram-bot-api';
+// import * as TelegramBot from 'node-telegram-bot-api';
 
 @Injectable()
 export class TelegramService {
-  private token: string;
-  public bot: TelegramBot;
+  // private token: string;
+  // public bot: TelegramBot;
 
-  constructor(private readonly configService: ConfigService) {
-    this.token = configService.get<string>('telegram.token');
-  }
+  // public init(): void {
+  //   this.bot = new TelegramBot(this.token, { polling: true });
 
-  public init(): void {
-    this.bot = new TelegramBot(this.token, { polling: true });
+  //   this.bot.on('message', (msg) => {
+  //     const chatId = msg.chat.id;
 
-    this.bot.on('message', (msg) => {
-      const chatId = msg.chat.id;
-
-      this.bot.sendMessage(chatId, 'Received your message');
-    });
-  }
+  //     this.bot.sendMessage(chatId, 'Received your message');
+  //   });
+  // }
 }
