584bb58d435e1c069618ca8ef328c4dabd208065
vitalii-zavhorodnii <vince.kkr@gmail.com>
2024-05-01 13:29:13 +0200

    added ton-ui transaction

--- apps/client/src/services/sendTonTransaction.ts
+++ apps/client/src/services/sendTonTransaction.ts
@@ -1,11 +1,13 @@
 import { tonConnectUI } from '@helpers/ton-connect-ui';
 
 export const sendTonTransaction = async (amount: number) => {
+  console.log({ amount });
+
   const transaction = {
     validUntil: Math.round(Date.now() / 1000) + 10,
     messages: [
       {
-        amount: String(amount),
+        amount: '1000000',
         address: '0:0000000000000000000000000000000000000000000000000000000000000000'
       }
     ]
