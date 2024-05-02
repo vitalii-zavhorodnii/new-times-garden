46e1b85686d00166777bc3155d7f47445a30de06
vitalii-zavhorodnii <vince.kkr@gmail.com>
2024-05-02 16:14:10 +0200

    ui menus updates

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -15,37 +15,37 @@ axios.defaults.baseURL = process.env.BACKEND_LINK;
 window?.Telegram?.WebApp?.expand();
 window?.Telegram?.WebApp?.enableClosingConfirmation();
 
-const overflow = 100;
-document.body.style.marginTop = `${overflow}px`;
-document.body.style.height = window.innerHeight + overflow + 'px';
-document.body.style.paddingBottom = `${overflow}px`;
-// window.scrollTo(0, overflow);
-document.getElementById('app').scrollIntoView();
+// const overflow = 100;
+// document.body.style.marginTop = `${overflow}px`;
+// document.body.style.height = window.innerHeight + overflow + 'px';
+// document.body.style.paddingBottom = `${overflow}px`;
+// // window.scrollTo(0, overflow);
+// document.getElementById('app').scrollIntoView();
 
-const scrollableEl = document.getElementById('app');
+// const scrollableEl = document.getElementById('app');
 
-let ts: number | undefined;
-const onTouchStart = (e: TouchEvent) => {
-  ts = e.touches[0].clientY;
-};
-const onTouchMove = (e: TouchEvent) => {
-  if (scrollableEl) {
-    const scroll = scrollableEl.scrollTop;
-    const te = e.changedTouches[0].clientY;
-    if (scroll <= 0 && ts! < te) {
-      e.preventDefault();
-    }
-  } else {
-    e.preventDefault();
-  }
-};
+// let ts: number | undefined;
+// const onTouchStart = (e: TouchEvent) => {
+//   ts = e.touches[0].clientY;
+// };
+// const onTouchMove = (e: TouchEvent) => {
+//   if (scrollableEl) {
+//     const scroll = scrollableEl.scrollTop;
+//     const te = e.changedTouches[0].clientY;
+//     if (scroll <= 0 && ts! < te) {
+//       e.preventDefault();
+//     }
+//   } else {
+//     e.preventDefault();
+//   }
+// };
 
-document.documentElement.addEventListener('touchstart', onTouchStart, {
-  passive: false
-});
-document.documentElement.addEventListener('touchmove', onTouchMove, {
-  passive: false
-});
+// document.documentElement.addEventListener('touchstart', onTouchStart, {
+//   passive: false
+// });
+// document.documentElement.addEventListener('touchmove', onTouchMove, {
+//   passive: false
+// });
 
 const screenSize = {
   x: Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0),
